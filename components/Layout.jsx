0d10f4233469d556628a803113fdee3a6fc5a8e7
badbishop02 <88867338+badbishop02@users.noreply.github.com>
2022-07-08 13:36:00 +0300

    Add files via upload

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Header from './Header';
+import { Header, Footer } from '../components';
 
 const Layout = ({ children }) => (
   <>
     <Header />
     {children}
+    <Footer />
   </>
   )
 
